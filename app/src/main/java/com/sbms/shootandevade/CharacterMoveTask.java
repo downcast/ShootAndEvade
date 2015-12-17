d51d3ff722691abd94174702249f3523e8805459
Marcus Smith <marcusmax94@yahoo.com>
2015-12-17 16:57:01 -0500

    Did stuff; the player now moves and can fire

--- app/src/main/java/com/sbms/shootandevade/CharacterMoveTask.java
+++ app/src/main/java/com/sbms/shootandevade/CharacterMoveTask.java
@@ -1,58 +1,70 @@
 package com.sbms.shootandevade;
 
 import android.os.AsyncTask;
-import android.widget.ImageView;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Class is used to make the character move up and down fluidly
  * @author Sherdon Brown
  */
-public class CharacterMoveTask extends AsyncTask<Void, Void, Void> {
+public class CharacterMoveTask extends AsyncTask<View, Void, Void> {
 
-	private ImageView charaImage;
-	private int valueX;
-	private int valueY;
-	private int screenHeight;
-	/** A higher value means the character moves slower */
-	private final int MOVESPEED= 25;
-
-	public CharacterMoveTask(ImageView characterImage, int moveX, int moveY, int screenHeight){
-		this.charaImage= characterImage;
-		this.valueX= moveX;
-		this.valueY= moveY;
-		this.screenHeight= screenHeight;
-	}
+	View mPlayer;
+	View mButton;
 
 	@Override
-	protected Void doInBackground(Void... ag0) {
-		// TODO Auto-generated method stub
-		while (!this.isCancelled()){
-			/*if (this.charaImage.getX() < 0){
-				this.charaImage.setyLocation(0);
-			}else if (this.charaImage.getImageYEdge() > screenHeight + 100){
-				this.charaImage.alignToLowerWall();
-			} else{
-				this.charaImage.updateYLocation(valueY);
-			}
-			///////////////////////////
-			if (this.charaImage.getxLocation() < 0){
-				this.charaImage.setxLocation(0);
-			}else if (this.charaImage.getImageXEdge() > screenHeight + 100){
-				this.charaImage.alignToLowerWall();
-			} else{
-				this.charaImage.updateXLocation(valueX);
-			}
+	protected Void doInBackground(View... arg) {
+		// Retrieve values
+		mPlayer = arg[0];
+		mButton = arg [1];
 
-			/////////////////////
-			*/
+		// Validate values
+		if (mPlayer == null || mButton == null){
+			Log.e("MoveTask", "Null argument");
+			return null;
+		}
+
+		// Run until user cancels task by not pressing button
+		// Sleep to slow down the movespeed of the player
+		while (!isCancelled()){
 			try {
-				Thread.sleep(MOVESPEED, 0);
+				publishProgress();
+				Thread.sleep(10, 0);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+
 		return null;
+	}
+
+	/** Code is run on the UI thread after a call to "publishProgress" */
+	@Override
+	protected void onProgressUpdate(Void... values) {
+		super.onProgressUpdate(values);
+
+		// Keep player from moving higher than the top of the screen
+		if (mPlayer.getY() < 0){
+			mPlayer.setY(0);
+			return;
+		}
+
+		// Keep player from moving below the bottom of the screen
+		if (mPlayer.getY() + mPlayer.getHeight() > MainActivity.sDisplayHeight){
+			mPlayer.setY(MainActivity.sDisplayHeight - mPlayer.getHeight());
+			return;
+		}
+
+		// Determine which button is pressed and adjust y-coordinate accordingly
+		switch (mButton.getId()){
+			case R.id.btn_controller_up:
+				mPlayer.setY(mPlayer.getY() - 30);
+				break;
+			case R.id.btn_controller_down:
+				mPlayer.setY(mPlayer.getY() + 30);
+				break;
+		}
 	}
 }
 
--- app/src/main/java/com/sbms/shootandevade/MainActivity.java
+++ app/src/main/java/com/sbms/shootandevade/MainActivity.java
@@ -1,22 +1,84 @@
 package com.sbms.shootandevade;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, View.OnClickListener{
+
+	RelativeLayout mLayoutGame;
+	Button mBtnControllerUp;
+	Button mBtnControllerDown;
+	Button mBtnControllerFire;
+	ImageView mPlayer;
+	ImageView mProjectile;
+
+	boolean mIsBtnControllerPressed;
+	CharacterMoveTask mMoveTask;
+
+	static int sDisplayHeight;
+	static int sDisplayWidth;
+	static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+	/** This is the space between the player and the projectile when it fires **/
+	final int PROJECTILE_MARGIN = 25;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+
+		// Grab the device height for the application to use elsewhere
+		sDisplayHeight = getResources().getDisplayMetrics().heightPixels;
+		// Grab the device width for the application to use elsewhere
+		sDisplayWidth = getResources().getDisplayMetrics().widthPixels;
+
+		// Link xml version of views to the code version
+		mLayoutGame = (RelativeLayout) findViewById(R.id.layout_game);
+		mBtnControllerUp = (Button) findViewById(R.id.btn_controller_up);
+		mBtnControllerDown = (Button) findViewById(R.id.btn_controller_down);
+		mBtnControllerFire = (Button) findViewById(R.id.btn_controller_fire);
+
+		// Create the view that represents the player
+		mPlayer = new ImageView(this);
+		mPlayer.setId(generateViewId());
+		// NOTE: the following is discouraged because it slows down the main thread.
+		// Create bitmap on another thread using the resource and pass it back
+		mPlayer.setImageResource(R.drawable.player_plane);
+
+		// Initialize the player movement task that will allow a fluid movement
+		mMoveTask = new CharacterMoveTask();
+
+		mIsBtnControllerPressed = false;
+
+		// These buttons will respond a touches
+		mBtnControllerUp.setOnTouchListener(this);
+		mBtnControllerDown.setOnTouchListener(this);
+
+		// These buttons will respond to clicks
+		mBtnControllerFire.setOnClickListener(this);
+
+		// Define parameters for the player starting location;
+		RelativeLayout.LayoutParams playerParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		playerParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+		playerParams.addRule(RelativeLayout.CENTER_VERTICAL);
+		playerParams.setMargins(10, 0, 0, 0);
+
+		mLayoutGame.addView(mPlayer, playerParams);
 	}
 
 	@Override
@@ -40,4 +102,60 @@ public class MainActivity extends AppCompatActivity {
 
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	public boolean onTouch(View view, MotionEvent event) {
+
+		switch (event.getAction()){
+			case MotionEvent.ACTION_DOWN:
+				mMoveTask = new CharacterMoveTask();
+				mMoveTask.execute(mPlayer, view);
+				break;
+			case MotionEvent.ACTION_UP:
+				// Stop the task so that the a new button can be used to determine if the player's y coordinate will rise or fall
+				if (!mMoveTask.cancel(true)){
+					Log.e("MoveTask", "Task cannot be canceled");
+				}
+				break;
+		}
+		return true;
+	}
+
+	@Override
+	public void onClick(View view) {
+		// It is assumed the only button that can be "clicked" is the 'mBtnControllerFire'
+
+		// Create the view that represents the projectile
+		mProjectile = new ImageView(this);
+		// NOTE: the following is discouraged because it slows down the main thread.
+		// Create bitmap on another thread using the resource and pass it back
+		mProjectile.setImageResource(R.drawable.player_plane);
+
+		// Set the starting coordinates of the projectile to the last position of the player
+		mProjectile.setX(mPlayer.getWidth() + PROJECTILE_MARGIN);
+		mProjectile.setY(mPlayer.getY());
+
+		mLayoutGame.addView(mProjectile);
+	}
+
+	/**
+	 * Generate a value suitable for use for View.setId().
+	 * This value will not collide with ID values generated at build time by aapt for R.id.
+	 * <br>
+	 *     Taken from the android source code for backward compatibility for API < 17.
+	 *
+	 * @return a generated ID value
+	 */
+	public static int generateViewId() {
+		for (;;) {
+			final int result = sNextGeneratedId.get();
+			// aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+			int newValue = result + 1;
+			if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+			if (sNextGeneratedId.compareAndSet(result, newValue)) {
+				return result;
+			}
+		}
+	}
+
 }
